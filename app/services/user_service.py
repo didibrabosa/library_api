05163cc1a7c3e26ee9f3af35298899717b29da76
Diego Vasconcelos <diegovasconcelosb@gmail.com>
2025-01-30 22:38:51 -0300

    style: improving the service layer

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -3,27 +3,28 @@ from typing import List
 from app.repositories.user_repository import UserRepository
 from app.entities.user import User
 
+
 class UserService:
-    def __init__(self):
+    def __init__(self, storage: UserRepository):
         self.logger = logging.getLogger(__name__)
-        self.repository = UserRepository()
-    
+        self.repository = storage
+
     def create_user(self, user: User):
         self.logger.info(f"Creating User with this data={user}")
         return self.repository.create_user(user)
-    
+
     def get_user_by_id(self, id: str) -> User:
         self.logger.info(f"Getting customer with this id={id}")
         return self.repository.get_user_by_id(id)
-    
+
     def get_all_users(self) -> List[User]:
         self.logger.info(f"Getting all Users")
         return self.repository.get_all_users()
-    
+
     def update_user(self, id: str, user: User) -> User:
         self.logger.info(f"Updated User with this id={id}")
         return self.repository.update_user(id, user)
-    
+
     def delete_user(self, id: str):
         self.logger.info(f"Deleting User with id={id}")
         self.repository.delete_user(id)
