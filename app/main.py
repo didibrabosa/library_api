63e61c29b2bdcef1ac2df689386496a10c04290b
Diego Vasconcelos <diegovasconcelosb@gmail.com>
2025-04-03 19:24:18 -0300

    feat: adding a lifespam to handle the project flow, injecting dependencies and improving health check

--- app/main.py
+++ app/main.py
@@ -1,12 +1,33 @@
+import logging
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from app.controller import user_controller
+from app.config.db import get_db_connection
+from app.services.user_service import UserService
+from app.repositories.user_repository import UserRepository
+from app.controller.user_controller import router
 
-app = FastAPI()
-
-
-@app.get("/")
-def root():
-    return {"message": "Library API is up and running!"}
+logger = logging.getLogger(__name__)
 
 
-app.include_router(user_controller.router)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    db_connection = get_db_connection()
+    user_repository = UserRepository(db_connection=db_connection)
+    user_service = UserService(user_repository)
+
+    yield {"user_service": user_service}
+
+    logger.info("Shutdown application")
+
+
+app = FastAPI(
+    lifespan=lifespan
+)
+
+
+app.include_router(router)
+
+
+@app.get("/health")
+def health_check():
+    return {"status": "healthy"}
